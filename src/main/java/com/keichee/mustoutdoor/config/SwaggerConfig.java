e3dbdf7a3a822ec3698b6320ef0dfa5a86c27fe2
khhwang838 <stork838@gmail.com>
2018-07-12 22:20:39 +0900

    swagger 적용

---
+++ src/main/java/com/keichee/mustoutdoor/config/SwaggerConfig.java
@@ -0,0 +1,11 @@
+/**
+ * Created on 2018. 7. 11. by Keichee
+ */
+package com.keichee.mustoutdoor.config;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+public class SwaggerConfig {
+
+}
