3be9ae2c334a15f8f6e8062e87944b656310890b
khhwang838 <stork838@gmail.com>
2018-07-22 11:08:21 +0900

    숙소-액티비티 타입 관계 dto 생성, 액티비티-액티비티 타입 관계 dto 생성

--- src/main/java/com/keichee/mustoutdoor/web/domain/acmd/dto/AcmdAcmdTypesRelDto.java
+++ src/main/java/com/keichee/mustoutdoor/web/domain/acmd/dto/AcmdAcmdTypesRelDto.java
@@ -5,7 +5,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class AcmdTypesRelDto {
+public class AcmdAcmdTypesRelDto {
     private String acmdTypeId;
     private String acmdUid;
     private String userId;
---
+++ src/main/java/com/keichee/mustoutdoor/web/domain/actv/dto/ActivityActivityTypesRelDto.java
@@ -0,0 +1,13 @@
+package com.keichee.mustoutdoor.web.domain.actv.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class ActivityActivityTypesRelDto {
+    private String actvTypeUid;
+    private String actvUid;
+}
