a621826d57763289131c2f22a38f63b7061504c7
khhwang838 <stork838@gmail.com>
2018-07-30 04:52:11 +0900

    Accommodation 컨트롤러 명 변경 -> AcmdController

--- src/main/java/com/keichee/mustoutdoor/web/controller/acmd/AcmdController.java
+++ src/main/java/com/keichee/mustoutdoor/web/controller/acmd/AcmdController.java
@@ -36,7 +36,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 @RequestMapping(value = "/acmd")
-public class AccommodationController {
+public class AcmdController {
 
 	@Autowired
 	private AcmdService acmdService;
